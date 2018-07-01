e60acc60949e2ef3e846aedb3d685dd59ef51014
Renzo Rivas <renzo.rivas@metova.com>
2018-06-30 21:52:30 -0500

    [python] Defining #compute for computing mutations between DNA strands

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,13 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(first_dna_strand, second_dna_strand):
+    if len(first_dna_strand) != len(second_dna_strand):
+        raise ValueError('DNA strands must be of equal length.')
+
+    number_of_mutations = 0
+    for index, char in enumerate(first_dna_strand):
+        if char != second_dna_strand[index]:
+            number_of_mutations += 1
+
+    return number_of_mutations
