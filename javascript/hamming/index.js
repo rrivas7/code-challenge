45dc84ef26900b2841f609a3c90277c83e4b623e
Renzo Rivas <renzo.rivas@metova.com>
2018-06-30 21:32:32 -0500

    [javascript] Creating Hammer for computing mutations between DNA strands

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -5,3 +5,15 @@
 
 
 module.exports = Hamming;
+
+function Hamming () {
+  this.compute = function(firstDNAStrand, secondDNAStrand) {
+    if (firstDNAStrand.length != secondDNAStrand.length) throw 'DNA strands must be of equal length.';
+
+    numberOfMutations = 0;
+    firstDNAStrand.split('').forEach(function(char, index) {
+      if (char != secondDNAStrand[index]) numberOfMutations++;
+    });
+    return numberOfMutations;
+  };
+}
